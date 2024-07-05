32daa732a519d55422768618200ba46368b8c0a3
AccelQuasarDragon <138998466+AccelQuasarDragon@users.noreply.github.com>
2024-07-04 20:55:06 -0700

    u

---
+++ kivyex/2024_7_5/main.py
@@ -0,0 +1,32 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+kv_string = '''
+BoxLayout:
+    orientation: 'vertical'
+    BoxLayout:
+        orientation: 'horizontal'
+        size_hint: (1, 0.2)
+        Button:
+            text: "start timer!"
+        Button:
+            text: "cancel timer"
+    BoxLayout:
+        orientation: 'horizontal'
+        Button:
+            text: "hours"
+        Button:
+            text: "minutes"
+        Button:
+            text: "seconds"
+'''
+
+class smApp(App):
+    def build(self):
+        return Builder.load_string(kv_string)
+
+if __name__ == '__main__':
+    #this is to make the Kivy window always on top
+    from kivy.core.window import Window
+    Window.always_on_top = True
+    smApp().run()
