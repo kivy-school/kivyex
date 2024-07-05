684365f66269d7c9cff54425fe4e37d5841bd0b5
AccelQuasarDragon <138998466+AccelQuasarDragon@users.noreply.github.com>
2024-07-05 13:40:26 -0700

    u

--- kivyex/2024_7_5/main.py
+++ kivyex/2024_7_5/main.py
@@ -15,27 +15,41 @@ kv_string = '''
     Button:
         size_hint: (1, 0.2)
         text: "timer only once"
+        on_release: root.start_timer_once()
     BoxLayout:
         orientation: 'horizontal'
         Button:
             id: "hoursbuttonID"
-            text: "30 seconds"
+            text: "Every 30 seconds: "
         Button:
             id: "minutesbuttonID"
-            text: "10 seconds"
+            text: "every 10 seconds: "
         Button:
             id: "secondsbuttonID"
-            text: "seconds"
+            text: "every second: "
+CustomBoxLayout:
 '''
 
 class CustomBoxLayout():
-    def start_timers(self, *args):
-        Clock.schedule_interval(updatecount, 1)
-        Clock.schedule_interval(updatecount, 10)
-        Clock.schedule_interval(updatecount, 30)
+    seconds = 0
+    ten_seconds = 0
+    thirty_seconds = 0
+
+    # def start_timers(self, *args):
+    #     Clock.schedule_interval(updatecount, 1)
+    #     Clock.schedule_interval(updatecountpartial, 10)
+    #     Clock.schedule_interval(updatecountlambda, 30)
 
     def start_timer_once(self, *args):
-        Clock.schedule_once(my_callback, 5)
+        Clock.schedule_once(self.updatecount, 5)
+
+    def updatecount(self, *args):
+        self.seconds += 1
+        self.ten_seconds += 1
+        self.thirty_seconds += 1
+    
+    def updatecountpartial(self, *args):
+    def updatecountlambda(self, *args):
 
 class smApp(App):
     def build(self):
